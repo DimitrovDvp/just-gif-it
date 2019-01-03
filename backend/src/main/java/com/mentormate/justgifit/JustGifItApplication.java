0229ea4ef87cbd831c0e4c068433df4437b26667
DimitrovDvp <infernalllll@mail.bg>
2019-01-03 14:45:53 +0200

    Init project

---
+++ backend/src/main/java/com/mentormate/justgifit/JustGifItApplication.java
@@ -0,0 +1,14 @@
+package com.mentormate.justgifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGifItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGifItApplication.class, args);
+	}
+
+}
+
